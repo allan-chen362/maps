5504d79b0d4dbdf35b810f500344d0bf3d172801
allan-chen362 <allanchen362@gmail.com>
2024-05-25 11:34:59 +0800

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+
+/// <reference types="@types/google.maps" />
+import { User } from "./User";
+import { Company } from "./company";
+
+
+
+new google.maps.Map(document.getElementById('map') as HTMLElement,{
+  zoom:1,
+  center:{
+    lat:0,
+    lng:0
+  }
+});
